4b022723464e857a204d652bb63212aa1d08a482
SergiySavarin <vir.host@gmail.com>
2016-01-14 20:51:36 +0200

    ticket:8 update1 tests

--- apps/contact/tests/test_custom_command.py
+++ apps/contact/tests/test_custom_command.py
@@ -1,6 +1,7 @@
 import subprocess
 
 from django.test import TestCase
+from fortytwo_test_task.settings import DATABASES
 
 
 class CustomCommand(TestCase):
@@ -8,6 +9,7 @@ class CustomCommand(TestCase):
     def test_custom_django_command_models_count(self):
         """Test for custom django command 'models_count'."""
         # run command models_count and take result like string from stdout
+        print DATABASES
         result = subprocess.check_output(
             ['python', 'manage.py', 'models_count'],
             stderr=subprocess.STDOUT
