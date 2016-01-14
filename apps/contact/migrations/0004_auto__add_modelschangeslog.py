bddcdd9f763a05c995af06d42b3ae407e561ae5d
SergiySavarin <vir.host@gmail.com>
2016-01-14 17:48:15 +0200

    ticket:8 add model for model actions log

---
+++ apps/contact/migrations/0004_auto__add_modelschangeslog.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'ModelsChangesLog'
+        db.create_table(u'contact_modelschangeslog', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('model_name', self.gf('django.db.models.fields.CharField')(max_length=128)),
+            ('action', self.gf('django.db.models.fields.CharField')(max_length=128)),
+            ('action_time', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'contact', ['ModelsChangesLog'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'ModelsChangesLog'
+        db.delete_table(u'contact_modelschangeslog')
+
+
+    models = {
+        u'contact.modelschangeslog': {
+            'Meta': {'object_name': 'ModelsChangesLog'},
+            'action': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'action_time': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model_name': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        u'contact.owner': {
+            'Meta': {'object_name': 'Owner'},
+            'bio': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'birthday': ('django.db.models.fields.DateField', [], {}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '256'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'jabber': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'other_info': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'photo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'skype': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        },
+        u'contact.usersrequest': {
+            'Meta': {'object_name': 'UsersRequest'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'request_str': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        }
+    }
+
+    complete_apps = ['contact']
