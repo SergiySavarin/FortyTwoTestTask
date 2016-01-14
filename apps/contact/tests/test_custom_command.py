3faeee696bf9a955acf5e6edb0df823b1879cc4b
SergiySavarin <vir.host@gmail.com>
2016-01-14 21:29:53 +0200

    ticket:8 update4 tests

--- apps/contact/tests/test_custom_command.py
+++ apps/contact/tests/test_custom_command.py
@@ -1,7 +1,6 @@
 import subprocess
 
-from django.core.urlresolvers import reverse
-from django.db import models
+from django.core.management import call_command
 from django.test import TestCase
 
 
@@ -10,11 +9,7 @@ class CustomCommand(TestCase):
     def test_custom_django_command_models_count(self):
         """Test for custom django command 'models_count'."""
         # run command models_count and take result like string from stdout
-        for app in models.get_apps():
-            for model in models.get_models(app):
-                print model.__name__
-        response = self.client.get(reverse('contact'))
-        self.assertEqual(response.status_code, 200)
+        call_command('models_count')
         result = subprocess.check_output(
             ['python', 'manage.py', 'models_count'],
             stderr=subprocess.STDOUT
