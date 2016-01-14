e59ad57c325293ab8f9b229de0be355484cdabe6
SergiySavarin <vir.host@gmail.com>
2016-01-14 18:32:53 +0200

    ticket:3 modify migrations

--- apps/contact/migrations/0002_auto__del_field_owner_other__add_field_owner_other_info.py
+++ apps/contact/migrations/0002_auto__del_field_owner_other__add_field_owner_other_info.py
@@ -8,12 +8,23 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding model 'Owner'
+        # Deleting field 'Owner.other'
+        db.delete_column(u'contact_owner', 'other')
+
+        # Adding field 'Owner.other_info'
+        db.add_column(u'contact_owner', 'other_info',
+                      self.gf('django.db.models.fields.TextField')(default='', blank=True),
+                      keep_default=False)
 
 
     def backwards(self, orm):
-        # Deleting model 'Owner'
-        db.delete_table(u'contact_owner')
+        # Adding field 'Owner.other'
+        db.add_column(u'contact_owner', 'other',
+                      self.gf('django.db.models.fields.TextField')(default='', blank=True),
+                      keep_default=False)
+
+        # Deleting field 'Owner.other_info'
+        db.delete_column(u'contact_owner', 'other_info')
 
 
     models = {
@@ -26,7 +37,7 @@ class Migration(SchemaMigration):
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'jabber': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
-            'other': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'other_info': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             'skype': ('django.db.models.fields.CharField', [], {'max_length': '256'})
         }
     }
