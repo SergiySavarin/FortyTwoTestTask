c70b8a81ee67ad9946cb20820aff337f934fa565
SergiySavarin <vir.host@gmail.com>
2016-01-14 21:16:47 +0200

    ticket:3 update3 tests

--- apps/contact/tests/test_custom_command.py
+++ apps/contact/tests/test_custom_command.py
@@ -1,5 +1,6 @@
 import subprocess
 
+from django.core.urlresolvers import reverse
 from django.db import models
 from django.test import TestCase
 
@@ -12,6 +13,8 @@ class CustomCommand(TestCase):
         for app in models.get_apps():
             for model in models.get_models(app):
                 print model.__name__
+        response = self.client.get(reverse('contact'))
+        self.assertEqual(response.status_code, 200)
         result = subprocess.check_output(
             ['python', 'manage.py', 'models_count'],
             stderr=subprocess.STDOUT
