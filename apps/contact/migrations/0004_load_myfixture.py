a0cb4d004a485003e2d3edf7945a81c806e2300c
SergiySavarin <vir.host@gmail.com>
2016-01-18 17:51:22 +0200

    ticket:5 update1 proj

---
+++ apps/contact/migrations/0004_load_myfixture.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        "Write your forwards methods here."
+        from django.core.management import call_command
+        call_command("loaddata", "initial_data.json")
+
+
+    def backwards(self, orm):
+        "Write your backwards methods here."
+
+    models = {
+        u'contact.owner': {
+            'Meta': {'object_name': 'Owner'},
+            'bio': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'birthday': ('django.db.models.fields.DateField', [], {}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '256'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'jabber': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'other_info': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'photo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'skype': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        },
+        u'contact.usersrequest': {
+            'Meta': {'object_name': 'UsersRequest'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'request_str': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        }
+    }
+
+    complete_apps = ['contact']
+    symmetrical = True
