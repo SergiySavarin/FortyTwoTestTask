b29f0a4df035dcdde13fe64bc5df81ad4fc7286e
SergiySavarin <vir.host@gmail.com>
2016-01-20 10:37:39 +0200

    ticket:5 add form init

--- apps/contact/forms.py
+++ apps/contact/forms.py
@@ -4,5 +4,9 @@ from .models import Owner
 
 class EditContactForm(forms.ModelForm):
     """Form for edit contact information page."""
+    def __init__(self, *args, **kwargs):
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Owner
--- apps/contact/views.py
+++ apps/contact/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponseBadRequest
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from resizeimg import size, resize
+from utils import size, resize
 
 from fortytwo_test_task.settings import MEDIA_ROOT
 from .models import Owner, UsersRequest
@@ -44,8 +44,8 @@ def edit_contact(request):
     if request.method == 'GET':
         form = EditContactForm(instance=owner)
         # style class for form fields
-        for field in form.fields:
-            form.fields[field].widget.attrs['class'] = 'form-control'
+        # for field in form.fields:
+        #     form.fields[field].widget.attrs['class'] = 'form-control'
         return render(request, 'edit_contact.html', {'form': form})
     elif request.method == 'POST':
         if request.POST.get('save_button') is not None:
