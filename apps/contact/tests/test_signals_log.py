0fcd7e5f899e8d21ea590cd9d407e56809b2b13d
SergiySavarin <vir.host@gmail.com>
2016-01-21 10:15:55 +0200

    ticket:8 remove if from signals test

--- apps/contact/tests/test_signals_log.py
+++ apps/contact/tests/test_signals_log.py
@@ -9,20 +9,11 @@ class ModelsChangesLoger(TestCase):
     def test_signlas_log(self):
         """Test site and contact.html content."""
         # test create action
-        def clear(log):
-            try:
-                log.objects.all().delete()
-                self.assertEqual(ModelsChangesLog.objects.count(), 0)
-                return True
-            except AssertionError as err:
-                print 'Assertion Error: ', err
-
-        if clear(ModelsChangesLog):
-            response = self.client.get(reverse('contact'))
-            self.assertEqual(response.status_code, 200)
-            obj = ModelsChangesLog.objects.first()
-            self.assertEqual(obj.model_name, 'UsersRequest')
-            self.assertEqual(obj.action, 'create')
+        response = self.client.get(reverse('contact'))
+        self.assertEqual(response.status_code, 200)
+        obj = ModelsChangesLog.objects.last()
+        self.assertEqual(obj.model_name, 'UsersRequest')
+        self.assertEqual(obj.action, 'create')
         # test edit action
         owner = Owner.objects.create(
             first_name='Vasja',
@@ -34,15 +25,13 @@ class ModelsChangesLoger(TestCase):
             jabber='vasja@nurilsk.com'
         )
         owner.save()
-        if clear(ModelsChangesLog):
-            owner.first_name = 'NotVasja'
-            owner.save()
-            obj = ModelsChangesLog.objects.first()
-            self.assertEqual(obj.model_name, 'Owner')
-            self.assertEqual(obj.action, 'edit')
+        owner.first_name = 'NotVasja'
+        owner.save()
+        obj = ModelsChangesLog.objects.last()
+        self.assertEqual(obj.model_name, 'Owner')
+        self.assertEqual(obj.action, 'edit')
         # test delete action
-        if clear(ModelsChangesLog):
-            owner.delete()
-            obj = ModelsChangesLog.objects.first()
-            self.assertEqual(obj.model_name, 'Owner')
-            self.assertEqual(obj.action, 'delete')
+        owner.delete()
+        obj = ModelsChangesLog.objects.last()
+        self.assertEqual(obj.model_name, 'Owner')
+        self.assertEqual(obj.action, 'delete')
