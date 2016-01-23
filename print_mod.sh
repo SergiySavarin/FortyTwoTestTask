cfb35791fecd138ac6e9263d5ca4f1730e96a3cc
SergiySavarin <vir.host@gmail.com>
2016-01-23 17:04:48 +0200

    ticket:13 update bash script

--- print_mod.sh
+++ print_mod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 FILENAME=$(date +"%Y-%m-%d")
 
-python manage.py models_count 2> $FILENAME.dat 1> /dev/null
+PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=fortytwo_test_task.settings django-admin.py models_count 2> $FILENAME.dat 1> /dev/null
